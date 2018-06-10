5d1a8560da1b29ad6953c67669641350c34fc9cc
munirkizilcali <munir.kizilcali@gmail.com>
2018-06-10 17:01:36 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
-
+// document.querySelector('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+document.querySelector('#save_lincoln').addEventListener('click', ()=>saveLincoln(parseInt(document.querySelector('#interval').value)))
 function saveLincoln(interval) {
   if(isNaN(interval)) {
     interval = 10000
+
   }
-  $('#foreground').fadeOut(interval);
+  document.querySelector('#foreground').style.opacity = 1
+  setInterval(()=>{document.querySelector('#foreground').style.opacity -= 0.01}, interval/100)
 }
